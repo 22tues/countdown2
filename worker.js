82729ce7f393a54ecd23f9671cc378ce6b3562f2
22 Tuesday <github.20260824@22tues.day>
2026-09-10 06:42:47 -0500

    Replace spaces with non-breaking spaces in countdown

--- worker.js
+++ worker.js
@@ -133,7 +133,7 @@ export default {
             const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
             const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
             const seconds = Math.floor((distance % (1000 * 60)) / 1000);
-            countdownEl.innerHTML = \`\${String(days).padStart(2, '0')}<span>Days</span><br/>\${String(hours).padStart(2, '0')}<span>Hours</span><br/>\${String(minutes).padStart(2, '0')}<span>Minutes</span><br/>\${String(seconds).padStart(2, '0')}<span>Seconds</span>\`;
+            countdownEl.innerHTML = \`\${String(days).padStart(2, '0')}&nbsp;<span>Days</span><br/>\${String(hours).padStart(2, '0')}&nbsp;<span>Hours</span><br/>\${String(minutes).padStart(2, '0')}&nbsp;<span>Minutes</span><br/>\${String(seconds).padStart(2, '0')}&nbsp;<span>Seconds</span>\`;
         }
         setInterval(updateCountdown, 1000);
         updateCountdown();
